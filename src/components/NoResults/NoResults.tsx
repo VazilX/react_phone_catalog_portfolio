335399f885833ce915ee7592d7833bd3391bb76a
Vasil Liakhivskyi <vazilx@gmail.com>
2024-07-16 09:30:10 +0200

    fix no-results link

--- src/components/NoResults/NoResults.tsx
+++ src/components/NoResults/NoResults.tsx
@@ -11,9 +11,8 @@ export const NoResults: React.FC<Props> = ({ categoryName }) => {
     <div className="no-results">
       <h1 className="no-results__title">{`${categoryName} not found`}</h1>
       <p className="no-results__suggest">
-        Would you like to see our <Link to="/shop/phone">phone</Link> or{' '}
-        <Link to="/shop/tablet">tablet</Link> catalog? Or just go to{' '}
-        <Link to="/">home page</Link>?
+        Would you like to see our <Link to="/shop/phones">phone</Link> catalog?
+        Or just go to <Link to="/">home page</Link>?
       </p>
     </div>
   );
