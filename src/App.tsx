3555b8c4062c26d63a1ff136c5447c4f8c483d1d
Vasil Liakhivskyi <vazilx@gmail.com>
2024-07-17 09:35:57 +0200

    fix scrol

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import './helpers/style/normalize.css';
 import './App.scss';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { ScrollToTop } from './components/ScrollToTop';
+import { MenuContext } from './helpers/utils/menuContext';
 
 export const App: React.FC = () => {
+  const { hasMenu } = useContext(MenuContext);
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+
+    if (hasMenu) {
+      body?.style.setProperty('position', 'fixed');
+    } else {
+      body?.style.setProperty('position', 'static');
+    }
+  }, [hasMenu]);
+
   return (
     <div className="app">
       <Header />
